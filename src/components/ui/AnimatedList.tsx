ec47f5d567123f04552fc628d031342a6ad81a5c
adrianofina <adrianophinah24@gmail.com>
2026-06-19 09:05:37 +0300

    debug

--- src/components/ui/AnimatedList.tsx
+++ src/components/ui/AnimatedList.tsx
@@ -19,7 +19,7 @@ interface AnimatedItemProps {
 
 const AnimatedItem = ({ children, delay = 0, index, onMouseEnter, onClick }: AnimatedItemProps) => {
   const ref = useRef(null);
-  const inView = useInView(ref, { amount: 0.5, triggerOnce: false });
+  const inView = useInView(ref, { amount: 0.5,  });
   return (
     <motion.div
       ref={ref}
